c50c5e37ec376782001b5df7fbd894188e0c72bf
uday <uday92327@gmail.com>
2026-03-07 02:22:04 +0530

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+//    git add : bring files from working directory to staging area
+//            "tells github to bring these files to staging area.. i want to commit them next"
+//
+//    git commit : bring these files from staging area to git history(repository)
+//        take a snapshot and gives u a id :
+//
+
+//    git add : select items
+//    git commit : finalize purchase
+//
+
+
+
+}
